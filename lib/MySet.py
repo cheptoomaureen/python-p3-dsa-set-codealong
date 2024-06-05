5a300a5adb0865a42e6f34cfd0903d6ec6d18ba7
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-06-05 10:56:05 +0300

    assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,26 @@
 class MySet:
+    def __init__(self,enumarable = []):
+        self.dictionary = {}
+        for value in enumarable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    def delete(self, value):
+        self.dictionary.pop(value)
+        return self
+    def size(self):
+        return len(self.dictionary)
+    def clear(self):
+        return self.dictionary.clear()
+    def __str__(self):
+        set_list = []
+        for key,value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+
 
     pass
